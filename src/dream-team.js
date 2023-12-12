abed637f854424c7f08857d78a1e699c29dbca5a
HNY_Badger <markelov-02@list.ru>
2023-12-12 16:17:06 +0400

    feat: finished dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let letters = [];
+  if (!members || !Array.isArray(members)){
+    return false;
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (!members[i] || typeof(members[i]) !== 'string') {
+      continue
+    }
+    let corrMember = members[i];
+    corrMember = corrMember.trim();
+    corrMember = corrMember.toUpperCase();
+    letters.push(corrMember.charAt(0));
+  }
+  return letters.sort().join().replaceAll(',', '');
 }
 
 module.exports = {
